26f203d5d43d899db0b042adf62d42b7a83238b6
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-05 19:52:22 +0200

    q2 done

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        if (a < b) {
-            int x = factorial(b) / factorial(a);
-            System.out.println(x % 10);
-        }else {
-            int y = factorial(a) / factorial(b);
-            System.out.println(y % 10);
-        }
-    }
-
-    public static int factorial(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        return n * factorial(n - 1);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+   
     }
 }
