88c8074c29078014d96d785a869e5968898a2086
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-06 02:25:47 +0200

    q3 test 1 good but not best

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-   
+        long n = sc.nextLong();
+        int res = 1;
+            for (long i = n; i > 0; i--) {
+                res *= 8;
+            }
+            System.out.println(res%10);
     }
 }
