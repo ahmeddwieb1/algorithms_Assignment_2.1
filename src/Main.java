64537cfd4c90368d55c47f2ce8eb4b73d7e06aed
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-06 02:35:32 +0200

    q3 now it 's the best

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int  n = sc.nextInt();
+        if (n == 0) {
+            System.out.println(1);
+            return;
+        }
         int pattern[] = {6, 8, 4, 2};
         int  index = n % 4;
         System.out.println(pattern[index]);
