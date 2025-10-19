38c04587a8f54d02519288f64d1d731a141ddcd1
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-19 21:18:56 +0200

    prime number 1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //factorization
-        int  n = sc.nextInt();
-        for(int i = 1; i <= n; i++) {
-            if (n%i == 0) {
-                System.out.print(i+" ");
+//        long n = sc.nextInt();
+        int n = 9;
+        int x = n;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i < Math.sqrt(n); i++) {
+
+            while (x % i == 0) {
+                x = x / i;
+                list.add(i);
             }
         }
-        System.out.println();
-        for (int i =1; i <= Math.sqrt(n); i++) {
-            if (n%i == 0) {
-                System.out.print(i+","+(n/i));
-                System.out.println();
-            }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i)+" ");
         }
-
-
     }
 }
