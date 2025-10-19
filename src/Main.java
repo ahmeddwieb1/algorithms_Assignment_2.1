d61d85f7e446c9523ca0ee252758115956a42110
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-20 00:01:15 +0200

    prime number final

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        long n = sc.nextInt();
-        int n = 9;
+        int n = sc.nextInt();
+//        int n = 14 ;
         int x = n;
         List<Integer> list = new ArrayList<>();
-        for (int i = 2; i < Math.sqrt(n); i++) {
+        for (int i = 2; i*i <= n; i++) {
 
             while (x % i == 0) {
                 x = x / i;
                 list.add(i);
             }
+        }
+        if (x!=1){
+            list.add(x);
         }
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i)+" ");
