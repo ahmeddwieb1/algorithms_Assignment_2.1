b6f940e70d4b5588a1ab658672a28829c752a647
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-21 22:31:47 +0200

    bad solve without now the idea of the soln

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-//        int n = 14 ;
-        int x = n;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 2; i*i <= n; i++) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+        int n = 1024;
+        int k = 5;
+        int array[] = new int[k];
+        // find i
+        //breack when k-1 !=0
+        for (int x = 2; x * x < n; x++) {//بيعد ال factors
+            for (int i = 0; i < k; ) {//بيعد لل array
+            if (array[k - 2] != 0) {
+                    array[k-1] = n;
+                    break;
+                }
+                while (n % x == 0 && array[k - 2] == 0) {
+                    array[i] = x;
+                    n /= x;
+                    i++;
+                }
 
-            while (x % i == 0) {
-                x = x / i;
-                list.add(i);
             }
         }
-        if (x!=1){
-            list.add(x);
+
+        for (int x = 0; x < k; x++) {
+            System.out.print(array[x] + " ");
         }
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i)+" ");
-        }
+
     }
 }
