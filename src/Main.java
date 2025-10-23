96cf70b10e56c8eb1add2e8d7098223deca0dd7a
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-23 06:16:01 +0200

    final result

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,48 @@ public class Main {
     public static void main(String[] args) {
 
 //        n = 100000000 ,75000000 ,50000000 ,20000000 ,10000000
-        int n[] ={ 10000000,50000000,100000000 };
-        int time[]= new int [n.length];
+        int n[] = {10000000, 50000000, 100000000};
+        int time[] = new int[n.length];
         for (int i = 0; i < n.length; i++) {
-            System.out.println("when array size ="+n[i]);
+            int array[] = generate_array(n[i]);
+            System.out.println("when array size =" + n[i]);
+
             long start = System.currentTimeMillis();
             System.out.println("start: " + start);
-            generate_array(n[i]);
+
+            System.out.println(linear_search(array, -1));
+
             long end = System.currentTimeMillis();
             System.out.println("end: " + end);
             System.out.println("all operation: " + (end - start) + "ms");
-            time[i] = (int)(end - start);
+            time[i] = (int) (end - start);
+            System.out.println();
         }
 //        cal the avg
-        int sum = 0 ;
+        int sum = 0;
         double avg = 0;
         for (int i = 0; i < time.length; i++) {
             sum += time[i];
-             avg = (double) sum /time.length;
+            avg = (double) sum / time.length;
         }
-        System.out.println("avg: "+avg);
+        System.out.println("avg: " + avg);
     }
 
-    public static void generate_array(int n) {
+    public static int[] generate_array(int n) {
         int array[] = new int[n];
         for (int i = 0; i < n; i++) {
             int random = (int) (Math.random() * n + 1);
             array[i] = random;
         }
+        return array;
+    }
+
+    public static int linear_search(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
