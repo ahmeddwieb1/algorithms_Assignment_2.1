4f95b79eba02d0f28b05dfa85604afd349aeb021
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-23 03:22:48 +0200

    cal avg for 3 number

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
 //        n = 100000000 ,75000000 ,50000000 ,20000000 ,10000000
         int n[] ={ 10000000,50000000,100000000 };
+        int time[]= new int [n.length];
         for (int i = 0; i < n.length; i++) {
             System.out.println("when array size ="+n[i]);
             long start = System.currentTimeMillis();
@@ -13,15 +12,23 @@ public class Main {
             long end = System.currentTimeMillis();
             System.out.println("end: " + end);
             System.out.println("all operation: " + (end - start) + "ms");
+            time[i] = (int)(end - start);
         }
+//        cal the avg
+        int sum = 0 ;
+        double avg = 0;
+        for (int i = 0; i < time.length; i++) {
+            sum += time[i];
+             avg = (double) sum /time.length;
+        }
+        System.out.println("avg: "+avg);
     }
 
-    public static int[] generate_array(int n) {
+    public static void generate_array(int n) {
         int array[] = new int[n];
         for (int i = 0; i < n; i++) {
             int random = (int) (Math.random() * n + 1);
             array[i] = random;
         }
-        return array;
     }
 }
