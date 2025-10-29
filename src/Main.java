4770076151b5fc51323393b53e95fd42f9b75068
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-29 18:42:47 +0200

    worms q

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int numOfshops = sc.nextInt();
-        int prices[] = new int[numOfshops];
-
-        for (int i = 0; i < prices.length; i++) {
-            prices[i] = sc.nextInt();
-        }
-        int numOfdays = sc.nextInt();
-        int Days[] = new int[numOfdays];
-        for (int i = 0; i < Days.length; i++) {
-            Days[i] = sc.nextInt();
-        }
-        Arrays.sort(prices);
-        for (int i = 0; i < Days.length; i++) {
-            System.out.println(upperBound(prices, Days[i]));
+        int num_of_piles = sc.nextInt();
+        int worms[] = new int[num_of_piles];
+        for (int i = 0; i < worms.length; i++) {
+            worms[i] = sc.nextInt();
         }
 
+        int number_of_juicy_worms = sc.nextInt();
+        int labels[] = new int[number_of_juicy_worms];
+        for (int i = 0; i < labels.length; i++) {
+            labels[i] = sc.nextInt();
+        }
+        for (int i = 1; i < worms.length; i++) {
+            worms[i] += worms[i - 1];
+        }
+        for (int i = 0; i < number_of_juicy_worms; i++) {
+            System.out.println(lowerBound(worms, labels[i]));
+        }
     }
 
-    public static int upperBound(int[] arr, int value) {
+    public static int lowerBound(int[] arr, int value) {
         int left = 0, right = arr.length;
         while (left < right) {
             int mid = (left + right) / 2;
-            if (arr[mid] <= value) {
+            if (arr[mid] < value) {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
-        return left;
+        return left + 1;
     }
 
 }
