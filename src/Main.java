9e30323063e6ef28d569c3740399947910b0efc0
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-10-29 02:06:30 +0200

    706B

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sizeA = sc.nextInt();
-        int sizeB = sc.nextInt();
 
-        int A[] = new int[sizeA];
-        int B[] = new int[sizeB];
+        int numOfshops = sc.nextInt();
+        int prices[] = new int[numOfshops];
 
-        for (int i = 0; i < A.length; i++) {
-            A[i] = sc.nextInt();
+        for (int i = 0; i < prices.length; i++) {
+            prices[i] = sc.nextInt();
         }
-        for (int i = 0; i < B.length; i++) {
-            B[i] = sc.nextInt();
+        int numOfdays = sc.nextInt();
+        int Days[] = new int[numOfdays];
+        for (int i = 0; i < Days.length; i++) {
+            Days[i] = sc.nextInt();
         }
-        Arrays.sort(A);
-        for (int i = 0; i < B.length; i++) {
-            System.out.println(binarysearch(A, B[i]) + " ");
+        Arrays.sort(prices);
+        for (int i = 0; i < Days.length; i++) {
+            System.out.println(upperBound(prices, Days[i]));
         }
+
     }
 
-    public static int binarysearch(int[] arr, int key) {
+    public static int upperBound(int[] arr, int value) {
         int left = 0, right = arr.length;
         while (left < right) {
             int mid = (left + right) / 2;
-            if (arr[mid] > key) {
-                right = mid ;
-            } else if (arr[mid] < key) {
+            if (arr[mid] <= value) {
                 left = mid + 1;
-            }else
-                return mid+1;
+            } else {
+                right = mid;
+            }
         }
-        return -1;
+        return left;
     }
 
 }
