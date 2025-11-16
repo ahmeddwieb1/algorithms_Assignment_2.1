aa46d0e0ddbd7e435d68c1b57c23d3f5f4586410
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-11-17 01:34:45 +0200

    frist try with q2 in assiment 3 it good but make need to make it dynamic

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,45 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-        int power = sc.nextInt();
-        System.out.println(power(num, power));
+        PriorityQueue<Job> jobs = new PriorityQueue<>(
+                (a, b) -> b.profit - a.profit
+        );
+        jobs.add(new Job("A", 2, 100));
+        jobs.add(new Job("B", 1, 19));
+        jobs.add(new Job("C", 2, 27));
+        jobs.add(new Job("D", 1, 25));
+        jobs.add(new Job("E", 3, 15));
+        int count = 0;
+        int sum = 0;
+        String array[] = new String[3];
+        while (!jobs.isEmpty()) {
+            Job job = jobs.poll();
+            if (job.DL > count) {
+                sum += job.profit;
+                array[count] = job.id;
+                count++;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] +" ");
+        }
+        System.out.println();
+        System.out.print(sum);
     }
+}
 
-    public static int power(int num, int pow) {
-        if (num == 0) {
-            return 1;
-        } else if(pow %2== 0) {
-            int temp = (int) Math.pow(num,pow/2);
-            return temp*temp;
-        }
-        else {
-            int temp = (int) Math.pow(num,(pow-1)/2);
-            return temp*temp*num;
-        }
+class Job {
+    int profit;
+    int DL;
+    String id;
+
+    Job(String id, int DL, int profit) {
+        this.id = id;
+        this.DL = DL;
+        this.profit = profit;
     }
 }
