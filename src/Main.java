5495db55151ddb02e82be7b8217efac6b5a03230
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-11-17 12:31:27 +0200

    q1 done

--- src/Main.java
+++ src/Main.java
@@ -4,74 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        PriorityQueue<Job> jobs = new PriorityQueue<>(
-                (a, b) -> b.profit - a.profit
-        );
-        boolean done = true;
-
-        while (done) {
-            System.out.println("1- Add Job");
-            System.out.println("2- Quit");
-            System.out.print("Enter choice: ");
-            int choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter Job ID: ");
-                    String id = sc.next();
-
-                    System.out.print("Enter Deadline: ");
-                    int dl = sc.nextInt();
-
-                    System.out.print("Enter Profit: ");
-                    int profit = sc.nextInt();
-
-                    jobs.add(new Job(id, dl, profit));
-                    System.out.println("Job Added!\n");
-                    break;
-
-                case 2:
-                    done = false;
-                    System.out.println("Exiting...");
-                    break;
-
-                default:
-                    System.out.println("Invalid choice! Try again.\n");
-            }
-        }
-
-        max_profit(jobs);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int paths =factorial(((n-1)+(m-1)))/(factorial(n-1)*factorial(m-1));
+        System.out.println(paths);
     }
-
-    public static void max_profit(PriorityQueue<Job> jobs) {
-        int count = 0;
-        int sum = 0;
-//can replace jobs.size with max DL it will work best but code wiil be more complex and
-// it will take more time to find the max DL and could be not effected like in ex1 the max is 4 and the output is 2
-        String array[] = new String[jobs.size()];
-        while (!jobs.isEmpty()) {
-            Job job = jobs.poll();
-            if (job.DL > count) {
-                sum += job.profit;
-                array[count] = job.id;
-                count++;
-            }
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
         }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println(sum);
-    }
-}
-
-class Job {
-    int profit;
-    int DL;
-    String id;
-
-    Job(String id, int DL, int profit) {
-        this.id = id;
-        this.DL = DL;
-        this.profit = profit;
+        return n * factorial(n - 1);
     }
 }
