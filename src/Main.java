9863443ad5b3a1f155dcffbcf08c9adcaccccc0b
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-11-24 13:25:58 +0200

    Recursion problem/D

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    static int mod = 1000000007;
-
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        fun(n);
     }
 
-    public static int add(int a, int b) {
-        return ((a % mod) + (b % mod) + mod) % mod;
-    }
-    public static int sub(int a, int b) {
-        return ((a % mod) - (b % mod) + mod) % mod;
-    }
-    public static int mul(int a, int b) {
-        return ((a % mod) * (b % mod)) % mod;
-    }
+    public static void fun(int a) {
 
-
+        System.out.print(a + " ");
+        if (a == 1) {
+            return;
+        }
+        if (a % 2 == 0) {
+            fun(a / 2);
+        } else {
+            fun((a * 3) + 1);
+        }
+    }
 }
