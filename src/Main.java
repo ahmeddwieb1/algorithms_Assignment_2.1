559c066b14d4a08eaca4545c9cc22f89bf827ee5
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-11-25 17:49:51 +0200

    apple division  problem E : i was near

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        fun(n);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        fun(arr);
     }
 
-    public static void fun(int a) {
-        System.out.print(a + " ");
-        if (a == 1) {
-            return;
+    public static void fun(int arr[]) {
+        int sum1 =0 ;
+        int sum2 =0 ;
+        Arrays.sort(arr);
+        for (int i = arr.length-1; i >= 0; i--) {
+            if(sum1>sum2){
+                sum2+=arr[i];
+            }
+            else {
+                sum1+=arr[i];
+            }
         }
-        if (a % 2 == 0) {
-            fun(a / 2);
-        } else {
-            fun((a * 3) + 1);
-        }
+        System.out.println(Math.abs(sum1-sum2));
     }
 }
