9fa605650643d95c76be33ccfa7ea659e9b13161
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-12-01 14:16:09 +0200

    230B problem N bad solve it work but crash in time limit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,54 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-//        double[] arr = {72, 96, 12, 18, 81, 20, 6, 2, 54, 1};
-        double[] arr = new double[n];
+        long[] arr = new long[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
+//            arr[i] = in.nextLong();
+            arr[i] = i + 1;
         }
-        for (int i = 0; i < arr.length ; i++) {
-            while (arr[i] % 2 == 0) {
-                arr[i] /= 2;
-            }
-            while (arr[i] % 3 == 0) {
-                arr[i] /= 3;
-            }
-            if (arr[i] != arr[0]) {
-                System.out.println("No");
-                return;
+        fun(arr);
+//        System.out.println(isprime(11881));
+    }
+
+    public static void fun(long[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+                if ((isprime(Math.sqrt(arr[i])))) {
+                    System.out.println("YES when "+ arr[i]);
+                }
+
+            HashSet x = fun1(arr[i]);
+            if (x.size() == 3)
+                System.out.println("YES when "+ arr[i]);
+            else
+                System.out.println("NO when "+ arr[i]);
+        }
+    }
+
+    public static HashSet fun1(long n) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                set.add(i);
             }
         }
-        System.out.println("Yes");
+        return set;
+    }
+
+    public static boolean isprime(double x) {
+        if (x < 2) {
+            return false;
+        }
+        for (int i = 2; i * i < x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
