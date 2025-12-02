c3503ef9845ff2920757570f925e0babad9cec2f
ahmeddwieb <ahmeddwieb713@gmail.com>
2025-12-02 20:13:20 +0200

    230B problem N(t-prime) done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
@@ -8,46 +6,26 @@ public class Main {
         int n = in.nextInt();
         long[] arr = new long[n];
         for (int i = 0; i < n; i++) {
-//            arr[i] = in.nextLong();
-            arr[i] = i + 1;
+            arr[i] = in.nextLong();
         }
         fun(arr);
-//        System.out.println(isprime(11881));
     }
 
     public static void fun(long[] arr) {
         for (int i = 0; i < arr.length; i++) {
-
-                if ((isprime(Math.sqrt(arr[i])))) {
-                    System.out.println("YES when "+ arr[i]);
-                }
-
-            HashSet x = fun1(arr[i]);
-            if (x.size() == 3)
-                System.out.println("YES when "+ arr[i]);
+            long x = (long) Math.sqrt(arr[i]);
+            if (x * x == arr[i] && isprime(x)) {
+                System.out.println("YES when " + arr[i]);
+            }
             else
-                System.out.println("NO when "+ arr[i]);
+                System.out.println("NO when " + arr[i]);
         }
     }
 
-    public static HashSet fun1(long n) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                set.add(i);
-            }
-        }
-        return set;
-    }
-
-    public static boolean isprime(double x) {
-        if (x < 2) {
-            return false;
-        }
-        for (int i = 2; i * i < x; i++) {
-            if (x % i == 0) {
-                return false;
-            }
+    public static boolean isprime(long x) {
+        if (x < 2) return false;
+        for (long i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
         }
         return true;
     }
